52ff2b8fb6128f588a8ceb376a9fd428ece8425a
David Perelroizen <david.perelroizen@gmail.com>
2022-08-01 09:10:34 +0200

    Postgresql db set-up

--- epicevent/epicevent/settings.py
+++ epicevent/epicevent/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'epicevent.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'epiceventdb',
+       'USER': 'epicpostgreuser',
+       'PASSWORD': 'Davidou2410$',
+       'HOST': '127.0.0.1',
+       'PORT': '5432',
     }
 }
 

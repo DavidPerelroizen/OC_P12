b855b8076ebb23e73ac96358f728c8f5cb34b723
David Perelroizen <david.perelroizen@gmail.com>
2022-08-01 14:22:38 +0200

    Finalized the user model

--- epicevent/authentication/models.py
+++ epicevent/authentication/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group
 
 # Create your models here.
+
+
+administration_group, created = Group.objects.get_or_create(name='administrators')
+sales_group, created = Group.objects.get_or_create(name='salesmen')
+support_group, created = Group.objects.get_or_create(name='supporters')
+
+group_choices = (
+    (administration_group.id, 'Administrators'),
+    (sales_group.id, 'Salesmen'),
+    (support_group.id, 'Supporters')
+)
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='authorization_group',
+                                 choices=group_choices)
+
+    USERNAME_FIELD = 'username'

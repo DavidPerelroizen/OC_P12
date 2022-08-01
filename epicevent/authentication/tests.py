ba293155fdc964a5fbb6e82d13c1d5aca0d0d32c
David Perelroizen <david.perelroizen@gmail.com>
2022-08-01 19:07:48 +0200

    Added model testing

--- epicevent/authentication/tests.py
+++ epicevent/authentication/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+import pytest
+from .models import User, administration_group
 
 # Create your tests here.
+
+
+@pytest.mark.django_db
+def test_user_model():
+    user = User.objects.create_user(username='david_test', password='davidou2410', group_id=administration_group)
+    expected_value = 'david_test, group administrators'
+    print(str(user))
+    assert str(user) == expected_value
+
+
+def test_user_creation_with_valid_data():
+    pass
+
+
+def test_user_creation_with_invalid_data():
+    pass

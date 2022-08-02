2df105d745a48cb86ca3e779c4e82db6015a2256
David Perelroizen <david.perelroizen@gmail.com>
2022-08-02 18:19:36 +0200

    Attempt to improve the tests

--- epicevent/authentication/tests.py
+++ epicevent/authentication/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 import pytest
 from .models import User, administration_group
+from django.urls import reverse
+from django.test import Client
 
 # Create your tests here.
 
@@ -13,9 +15,18 @@ def test_user_model():
     assert str(user) == expected_value
 
 
+@pytest.mark.django_db
 def test_user_creation_with_valid_data():
-    pass
+    client = Client()
+    url = 'http://127.0.0.1:8000/api/users/sign-up/'
+    form_data = {'username': 'david_test', 'password': 'davidou2410', 'group_id': 'administrators'}
+    response = client.post(url, data=form_data)
+    expected_value = 'david_test, group administrators'
+    print(response)
+    assert response.status_code == 201
+    assert str(response) == expected_value
 
 
+@pytest.mark.django_db
 def test_user_creation_with_invalid_data():
     pass

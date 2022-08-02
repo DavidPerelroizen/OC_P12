b9c836a4ced73377e93b74847a1b2b78e18f54b1
David Perelroizen <david.perelroizen@gmail.com>
2022-08-02 18:19:10 +0200

    Added a Group serializer. Included the group serializer into the user serializer

--- epicevent/authentication/serializers.py
+++ epicevent/authentication/serializers.py
@@ -1,17 +1,24 @@
 from rest_framework.serializers import ModelSerializer, CharField, EmailField
 from .models import User
+from django.contrib.auth.models import Group
+
+
+class GroupSerializer(ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['name']
 
 
 class UserSerializer(ModelSerializer):
+    authorization_group = GroupSerializer(many=True, required=True)
     username = CharField(required=True)
     password = CharField(required=True, min_length=8, write_only=True)
-    group_id = CharField(required=True)
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], group_id=validated_data['group_id'],
+        user = User.objects.create_user(validated_data['username'], group_id=validated_data['authorization_group'],
                                         password=validated_data['password'])
         return user
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'group_id']
+        fields = ['username', 'password', 'authorization_group']

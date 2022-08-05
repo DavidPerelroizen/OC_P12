063824112b20746771c9c82cb280d46977f830a5
David Perelroizen <david.perelroizen@gmail.com>
2022-08-05 16:09:18 +0200

    User class modification

--- epicevent/authentication/models.py
+++ epicevent/authentication/models.py
@@ -8,20 +8,8 @@ administration_group, created = Group.objects.get_or_create(name='administrators
 sales_group, created = Group.objects.get_or_create(name='salesmen')
 support_group, created = Group.objects.get_or_create(name='supporters')
 
-group_choices = (
-    (administration_group, 'Administrators'),
-    (sales_group, 'Salesmen'),
-    (support_group, 'Supporters')
-)
-
 
 class User(AbstractUser):
-    username = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=100)
-    group_id = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='authorization_group',
-                                 choices=group_choices)
 
     def __str__(self):
-        return f'{self.username}, group {self.group_id}'
-
-    USERNAME_FIELD = 'username'
+        return f'{self.username}, group {self.groups.first().name}'

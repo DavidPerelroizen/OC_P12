3713362f342e316e324aa82a609acc2c81ff2cd6
David Perelroizen <david.perelroizen@gmail.com>
2022-08-05 10:44:09 +0200

    Added API test cases

--- epicevent/authentication/tests.py
+++ epicevent/authentication/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 import pytest
 from .models import User, administration_group
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.test import Client
+from rest_framework.test import APITestCase
 
 # Create your tests here.
 
@@ -30,3 +31,20 @@ def test_user_creation_with_valid_data():
 @pytest.mark.django_db
 def test_user_creation_with_invalid_data():
     pass
+
+
+class TestUserManagement(APITestCase):
+
+    url = 'http://127.0.0.1:8000/api/users/user_management/'
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_create(self):
+        form_data = {'username': 'david_test', 'password': 'davidou2410', 'group_id': 'administrators'}
+        response = self.client.post(self.url, data=form_data)
+        self.assertEqual(response.status_code, 200)
+        expected_result = 'david_test, group administrators'
+        user = User.objects.get.all()[0]
+        self.assertEqual(str(user), expected_result)

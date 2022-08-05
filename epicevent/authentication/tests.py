7e8555cad167c404b484dbed2b6d9513b213c9fb
David Perelroizen <david.perelroizen@gmail.com>
2022-08-05 16:09:58 +0200

    Improved tests

--- epicevent/authentication/tests.py
+++ epicevent/authentication/tests.py
@@ -4,33 +4,23 @@ from .models import User, administration_group
 from django.urls import reverse, reverse_lazy
 from django.test import Client
 from rest_framework.test import APITestCase
+from django.contrib.auth.models import Group
 
 # Create your tests here.
 
 
 @pytest.mark.django_db
 def test_user_model():
-    user = User.objects.create_user(username='david_test', password='davidou2410', group_id=administration_group)
+    administration_group, created = Group.objects.get_or_create(name='administrators')
+    sales_group, created = Group.objects.get_or_create(name='salesmen')
+    support_group, created = Group.objects.get_or_create(name='supporters')
+
+    user = User.objects.create_user(username='david_test', password='davidou2410')
+    user_group = Group.objects.get(id=1)
+    user.groups.add(user_group.id)
     expected_value = 'david_test, group administrators'
     print(str(user))
     assert str(user) == expected_value
-
-
-@pytest.mark.django_db
-def test_user_creation_with_valid_data():
-    client = Client()
-    url = 'http://127.0.0.1:8000/api/users/sign-up/'
-    form_data = {'username': 'david_test', 'password': 'davidou2410', 'group_id': 'administrators'}
-    response = client.post(url, data=form_data)
-    expected_value = 'david_test, group administrators'
-    print(response)
-    assert response.status_code == 201
-    assert str(response) == expected_value
-
-
-@pytest.mark.django_db
-def test_user_creation_with_invalid_data():
-    pass
 
 
 class TestUserManagement(APITestCase):
@@ -42,9 +32,13 @@ class TestUserManagement(APITestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_create(self):
-        form_data = {'username': 'david_test', 'password': 'davidou2410', 'group_id': 'administrators'}
+        administration_group, created = Group.objects.get_or_create(name='administrators')
+        sales_group, created = Group.objects.get_or_create(name='salesmen')
+        support_group, created = Group.objects.get_or_create(name='supporters')
+
+        form_data = {'username': 'david_test', 'password': 'davidou2410', 'groups': 'administrators'}
         response = self.client.post(self.url, data=form_data)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 201)
         expected_result = 'david_test, group administrators'
-        user = User.objects.get.all()[0]
+        user = User.objects.all()[0]
         self.assertEqual(str(user), expected_result)

4f061f6ccd3c3caa755dde53f4a5a15a839974a3
David Perelroizen <david.perelroizen@gmail.com>
2022-08-09 14:39:25 +0200

    updated urls names

--- epicevent/events/urls.py
+++ epicevent/events/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 router = routers.SimpleRouter()
 
 router.register('client_management', ClientManagement, basename='client_management')
-router.register('contract_management', ContractManagement, basename='Contract_management')
+router.register('contract_management', ContractManagement, basename='contract_management')
 router.register('event_management', EventManagement, basename='event_management')
 
 urlpatterns = [

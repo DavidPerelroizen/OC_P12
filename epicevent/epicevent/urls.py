25c1eb23a2fafe86e9183ac7830a36d37f45208b
David Perelroizen <david.perelroizen@gmail.com>
2022-08-09 12:32:30 +0200

    Added the router for events urls in the global url file

--- epicevent/epicevent/urls.py
+++ epicevent/epicevent/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/users/', include('authentication.urls')),
+    path('api/controller/', include('events.urls')),
 ]

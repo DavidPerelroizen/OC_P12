458908b52e6bfad6234b5302effccd50e7b0dbeb
David Perelroizen <david.perelroizen@gmail.com>
2022-08-09 12:32:44 +0200

    Created the events urls

---
+++ epicevent/events/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from .views import ContractManagement, ClientManagement, EventManagement
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+
+router.register('client_management', ClientManagement, basename='client_management')
+router.register('contract_management', ContractManagement, basename='Contract_management')
+router.register('event_management', EventManagement, basename='event_management')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]

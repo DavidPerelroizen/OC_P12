a8272498b64d59b24228078d466327b0717258e8
David Perelroizen <david.perelroizen@gmail.com>
2022-08-09 12:32:58 +0200

    Initiated the modelviewsets

--- epicevent/events/views.py
+++ epicevent/events/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import EventSerializer, EventStatusSerializer, ClientSerializer, ContractSerializer
+from .models import Client, Event, EventStatus, Contract
 
 # Create your views here.
+
+
+class ContractManagement(ModelViewSet):
+    serializer_class = ContractSerializer
+
+    def get_queryset(self):
+        return Contract.objects.all()
+
+
+class ClientManagement(ModelViewSet):
+    serializer_class = ClientSerializer
+
+    def get_queryset(self):
+        return Client.objects.all()
+
+
+class EventManagement(ModelViewSet):
+    serializer_class = EventSerializer
+
+    def get_queryset(self):
+        return Event.objects.all()

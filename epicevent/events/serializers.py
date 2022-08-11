b277e0e4fb272e6d878072c6c0733b2215d25cde
David Perelroizen <david.perelroizen@gmail.com>
2022-08-11 15:25:33 +0200

    Added create and update methods to the ClientCustomer serializer

--- epicevent/events/serializers.py
+++ epicevent/events/serializers.py
@@ -1,5 +1,8 @@
-from rest_framework.serializers import ModelSerializer
+import datetime
+
+from rest_framework.serializers import ModelSerializer, CharField, EmailField, IntegerField, DateTimeField
 from .models import ClientCustomer, Event, EventStatus, Contract
+from authentication import serializers, models
 
 
 class EventStatusSerializer(ModelSerializer):
@@ -9,10 +12,47 @@ class EventStatusSerializer(ModelSerializer):
 
 
 class ClientSerializer(ModelSerializer):
+    first_name = CharField(required=True)
+    last_name = CharField(required=True)
+    email = EmailField(required=True)
+    phone = CharField(required=True)
+    mobile = CharField(required=True)
+    company_name = CharField(required=True)
+    date_created = DateTimeField(required=False)
+    date_updated = DateTimeField(required=False)
+    sales_contact = serializers.UserSerializer(required=False)
+    sales_contact_name = CharField(required=False)
+
+    def create(self, validated_data):
+        client_customer = ClientCustomer()
+        client_customer.first_name = validated_data['first_name']
+        client_customer.last_name = validated_data['last_name']
+        client_customer.email = validated_data['email']
+        client_customer.phone = validated_data['phone']
+        client_customer.mobile = validated_data['mobile']
+        client_customer.company_name = validated_data['company_name']
+        client_customer.date_created = datetime.datetime.now()
+        client_customer.date_updated = datetime.datetime.now()
+        client_customer.sales_contact = models.User.objects.get(username=validated_data['sales_contact_name'])
+        client_customer.save()
+        return client_customer
+
+    def update(self, client_customer, validated_data):
+        client_customer.first_name = validated_data['first_name']
+        client_customer.last_name = validated_data['last_name']
+        client_customer.email = validated_data['email']
+        client_customer.phone = validated_data['phone']
+        client_customer.mobile = validated_data['mobile']
+        client_customer.company_name = validated_data['company_name']
+        client_customer.date_updated = datetime.datetime.now()
+        client_customer.sales_contact = models.User.objects.get(username=validated_data['sales_contact_name'])
+        client_customer.save()
+        return client_customer
+
     class Meta:
         model = ClientCustomer
         fields = ['first_name', 'last_name', 'email', 'phone', 'mobile', 'company_name', 'date_created',
-                  'date_updated', 'sales_contact']
+                  'date_updated', 'sales_contact', 'sales_contact_name']
 
 
 class EventSerializer(ModelSerializer):

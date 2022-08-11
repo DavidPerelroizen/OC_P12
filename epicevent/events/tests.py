fc9501e5812304fa8e7355e1e787010a0bb26958
David Perelroizen <david.perelroizen@gmail.com>
2022-08-11 11:22:05 +0200

    Initiated tests for events

--- epicevent/events/tests.py
+++ epicevent/events/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+import pytest
+from .models import Contract, Client, Event, EventStatus
+from django.urls import reverse, reverse_lazy
+from django.test import Client
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import Group
+from django.shortcuts import get_object_or_404
 
 # Create your tests here.
+
+
+class TestClientManagement(APITestCase):
+
+    url = 'http://127.0.0.1:8000/api/controller/client_management/'
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_create(self):
+        pass
+
+    def test_update(self):
+        pass
+
+    def test_delete(self):
+        pass

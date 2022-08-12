6b7d510dbc970a00b546bdd4cc4d261fd1b0df37
David Perelroizen <david.perelroizen@gmail.com>
2022-08-12 19:57:41 +0200

    Added test for event deletion

--- epicevent/events/tests.py
+++ epicevent/events/tests.py
@@ -176,7 +176,81 @@ class TestEventManagement(APITestCase):
         self.assertEqual(event_created.client_customer.id, form_data_event['client_customer'])
 
     def test_update(self):
-        pass
+        # Step 1: groups and user creation
+        user = fixture_group_and_user_creation('supporters')
+
+        # Step 2: create ClientCustomer
+        response = self.client.post(self.url_client, data=self.form_data_client)
+        self.assertEqual(response.status_code, 201)
+        client_created = ClientCustomer.objects.all()[0]
+
+        # Step 3: create EventStatus
+        event_status = EventStatus.objects.get_or_create(name='event_status_name', status_is_active=True)[0]
+
+        # Step 4: create Event
+        form_data_event = {'event_status': event_status.id, 'attendees': 200, 'event_date': datetime.datetime.now(),
+                           'notes': 'Some notes', 'support_contact': user.id,
+                           'client_customer': client_created.id}
+        response_event = self.client.post(self.url, data=form_data_event)
+
+        self.assertEqual(response_event.status_code, 201)
+
+        event_created = Event.objects.all()[0]
+
+        self.assertEqual(event_created.event_status.id, form_data_event['event_status'])
+        self.assertEqual(event_created.attendees, form_data_event['attendees'])
+        self.assertNotEqual(event_created.event_date, '')
+        self.assertEqual(event_created.notes, form_data_event['notes'])
+        self.assertEqual(event_created.support_contact.id, form_data_event['support_contact'])
+        self.assertEqual(event_created.client_customer.id, form_data_event['client_customer'])
+
+        # Step 5: update Event
+        form_data_update = {'event_status': event_status.id, 'attendees': 400, 'event_date': '2022-09-04T18:37:00Z',
+                            'notes': 'Some more notes', 'support_contact': user.id,
+                            'client_customer': client_created.id}
+        url_for_update = self.url + f'{event_created.id}/'
+
+        response_update = self.client.put(url_for_update, data=form_data_update)
+
+        self.assertEqual(response_update.status_code, 200)
+        self.assertEqual(event_created.event_status.id, form_data_update['event_status'])
+        self.assertEqual(event_created.attendees, form_data_update['attendees'])
+        self.assertEqual(event_created.event_date, form_data_update['event_date'])
+        self.assertEqual(event_created.notes, form_data_update['notes'])
+        self.assertEqual(event_created.support_contact.id, form_data_update['support_contact'])
+        self.assertEqual(event_created.client_customer.id, form_data_update['client_customer'])
 
     def test_delete(self):
-        pass
+        # Step 1: groups and user creation
+        user = fixture_group_and_user_creation('supporters')
+
+        # Step 2: create ClientCustomer
+        response = self.client.post(self.url_client, data=self.form_data_client)
+        self.assertEqual(response.status_code, 201)
+        client_created = ClientCustomer.objects.all()[0]
+
+        # Step 3: create EventStatus
+        event_status = EventStatus.objects.get_or_create(name='event_status_name', status_is_active=True)[0]
+
+        # Step 4: create Event
+        form_data_event = {'event_status': event_status.id, 'attendees': 200, 'event_date': datetime.datetime.now(),
+                           'notes': 'Some notes', 'support_contact': user.id,
+                           'client_customer': client_created.id}
+        response_event = self.client.post(self.url, data=form_data_event)
+
+        self.assertEqual(response_event.status_code, 201)
+
+        event_created = Event.objects.all()[0]
+
+        self.assertEqual(event_created.event_status.id, form_data_event['event_status'])
+        self.assertEqual(event_created.attendees, form_data_event['attendees'])
+        self.assertNotEqual(event_created.event_date, '')
+        self.assertEqual(event_created.notes, form_data_event['notes'])
+        self.assertEqual(event_created.support_contact.id, form_data_event['support_contact'])
+        self.assertEqual(event_created.client_customer.id, form_data_event['client_customer'])
+
+        # Step 5: delete Event
+        url_for_deletion = self.url + f'{event_created.id}/'
+        response = self.client.delete(url_for_deletion)
+        self.assertEqual(response.status_code, 204)
+

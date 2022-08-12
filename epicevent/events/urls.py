8280682b7d173cb4e9f03a8f2821f3863046d5fa
David Perelroizen <david.perelroizen@gmail.com>
2022-08-12 19:12:30 +0200

    Added the path related to the EventStatus management

--- epicevent/events/urls.py
+++ epicevent/events/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import ContractManagement, ClientManagement, EventManagement
+from .views import ContractManagement, ClientManagement, EventManagement, EventStatusManagement
 from rest_framework import routers
 
 router = routers.SimpleRouter()
@@ -7,6 +7,7 @@ router = routers.SimpleRouter()
 router.register('client_management', ClientManagement, basename='client_management')
 router.register('contract_management', ContractManagement, basename='contract_management')
 router.register('event_management', EventManagement, basename='event_management')
+router.register('eventstatus_management',EventStatusManagement, basename='eventstatus_management')
 
 urlpatterns = [
     path('', include(router.urls)),

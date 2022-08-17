90b13e7112eef8585a3ab31f91d18185d8bbaaf5
David Perelroizen <david.perelroizen@gmail.com>
2022-08-17 16:46:24 +0200

    Added permission to user management view

--- epicevent/authentication/views.py
+++ epicevent/authentication/views.py
@@ -5,11 +5,13 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
 from .models import User
+from .permissions import CanManageUserCreation
 # Create your views here.
 
 
 class UserManagement(ModelViewSet):
     serializer_class = UserSerializer
+    permission_classes = [CanManageUserCreation]
 
     def get_queryset(self):
         return User.objects.all()

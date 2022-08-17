4f9f370aa1e0c5f0bf0c453b87fbab16f7390663
David Perelroizen <david.perelroizen@gmail.com>
2022-08-17 19:15:41 +0200

    Added space

--- epicevent/events/urls.py
+++ epicevent/events/urls.py
@@ -7,7 +7,7 @@ router = routers.SimpleRouter()
 router.register('client_management', ClientManagement, basename='client_management')
 router.register('contract_management', ContractManagement, basename='contract_management')
 router.register('event_management', EventManagement, basename='event_management')
-router.register('eventstatus_management',EventStatusManagement, basename='eventstatus_management')
+router.register('eventstatus_management', EventStatusManagement, basename='eventstatus_management')
 
 urlpatterns = [
     path('', include(router.urls)),

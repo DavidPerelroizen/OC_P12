d3ddf36b77f7f27785040cfe2bbb4d378bb70cfc
David Perelroizen <david.perelroizen@gmail.com>
2022-08-17 16:53:06 +0200

    Added permission to user management view to the administration_group

--- epicevent/authentication/models.py
+++ epicevent/authentication/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group
+from .permissions import CanManageUserCreation
 
 # Create your models here.
 
@@ -7,12 +8,15 @@ from django.contrib.auth.models import AbstractUser, Group
 Instruction for readme: comment the three lines below before making the first migration.
 After the first migration, de-comment them and migrate.
 '''
-
+# Groups creation
 administration_group, created = Group.objects.get_or_create(name='administrators')
 sales_group, created = Group.objects.get_or_create(name='salesmen')
 support_group, created = Group.objects.get_or_create(name='supporters')
 
 groups_names_list = [administration_group.name, sales_group.name, support_group.name]
+
+# Groups permissions set-up
+administration_group.permissions.add(CanManageUserCreation)
 
 
 class User(AbstractUser):

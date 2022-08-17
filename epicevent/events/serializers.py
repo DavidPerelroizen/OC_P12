902750aa96344d063684b1b1f9e439ba79afe8d8
David Perelroizen <david.perelroizen@gmail.com>
2022-08-17 10:40:42 +0200

    Added validators to ContractSerializer and modified contract_status field to BooleanField

--- epicevent/events/serializers.py
+++ epicevent/events/serializers.py
@@ -1,7 +1,7 @@
 import datetime
 
 from rest_framework.serializers import ModelSerializer, CharField, EmailField, IntegerField, DateTimeField, FloatField,\
-    ValidationError
+    ValidationError, BooleanField
 from .models import ClientCustomer, Event, EventStatus, Contract
 from authentication import serializers, models
 
@@ -57,10 +57,22 @@ class ContractSerializer(ModelSerializer):
     date_created = DateTimeField(required=False)
     date_updated = DateTimeField(required=False)
     amount = FloatField(required=True)
-    contract_status = CharField(required=False)  # Contract not signed = False, Contract signed = True
+    contract_status = BooleanField(required=False)  # Contract not signed = False, Contract signed = True
     payment_due_date = DateTimeField(required=True)
 
     class Meta:
         model = Contract
         fields = ['date_created', 'date_updated', 'amount', 'contract_status', 'payment_due_date', 'client_customer',
                   'sales_contact']
+
+    def validate_client_customer(self, client_customer):
+        test_client_customer = ClientCustomer.objects.get(id=client_customer.id)
+        if test_client_customer is None:
+            raise ValidationError('The selected client does not exist')
+        return client_customer
+
+    def validate_sales_contact(self, sales_contact):
+        test_sales_contact = models.User.objects.get(id=sales_contact.id)
+        if test_sales_contact.description != f'{test_sales_contact.username}, group salesmen':
+            raise ValidationError('The selected sales contact does not belong to the salesmen group')
+        return sales_contact

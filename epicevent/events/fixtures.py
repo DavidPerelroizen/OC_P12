e856de597f3db1d195f663dcc6ea497e8c1e966f
David Perelroizen <david.perelroizen@gmail.com>
2022-08-17 09:35:24 +0200

    Modified the fixture so that it would take three arguments instead of one.

--- epicevent/events/fixtures.py
+++ epicevent/events/fixtures.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import Group
 from authentication import models
 
 
-def fixture_group_and_user_creation(group_name):
+def fixture_group_and_user_creation(group_name, user_name, pass_word):
     administration_group, created = Group.objects.get_or_create(name='administrators')
     sales_group, created = Group.objects.get_or_create(name='salesmen')
     support_group, created = Group.objects.get_or_create(name='supporters')
 
-    user = models.User.objects.create_user(username='david_test', password='davidou2410')
+    user = models.User.objects.create_user(username=user_name, password=pass_word)
     user_group = Group.objects.get(name=group_name)
     user.groups.add(user_group.id)
 

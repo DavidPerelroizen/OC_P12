5152df4f382094368c9a44d41acea73a4ecfc80c
David Perelroizen <david.perelroizen@gmail.com>
2022-08-19 09:56:52 +0200

    Added Client, Contract, Event, EventStatus models to admin site

--- epicevent/events/admin.py
+++ epicevent/events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ClientCustomer, Contract, Event, EventStatus
 
 # Register your models here.
+
+admin.site.register(ClientCustomer)
+admin.site.register(Contract)
+admin.site.register(Event)
+admin.site.register(EventStatus)
